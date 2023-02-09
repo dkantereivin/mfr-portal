208b6609e4e5c8ef718a3ff947350883205b601e
David Kanter Eivin <david.kantereivin@sjacs.ca>
2023-02-09 00:45:57 -0500

    feat: try Vercel for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import cloudflare from '@sveltejs/adapter-cloudflare';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: cloudflare()
+		adapter: vercel()
 	}
 };
 
