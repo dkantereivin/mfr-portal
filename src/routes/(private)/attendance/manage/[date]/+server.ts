e158a12999d18b9939c92a66a0a7ba2ac280e167
David Kanter Eivin <david.kantereivin@sjacs.ca>
2023-02-09 00:38:53 -0500

    feat: update with latest features to fix deployment

---
+++ src/routes/(private)/attendance/manage/[date]/+server.ts
@@ -0,0 +1,42 @@
+import type { RequestHandler } from "./$types";
+import { db } from "$lib/server/db";
+import { requireRank } from "$lib/utils/auth";
+import {Role, LeadershipDepartment} from "@prisma/client";
+import dayjs from "dayjs";
+import {json} from '@sveltejs/kit';
+
+// TODO: move to form action
+export const PUT = (async ({request, locals}) => {
+    requireRank(
+        locals.user!, Role.CORPORAL,
+        [LeadershipDepartment.ADMINISTRATION, LeadershipDepartment.TRAINING]
+    );
+
+    const {memberId, time} = await request.json();
+    const date = dayjs().set('date', dayjs(time).date());
+    const attendance = await db.attendance.create({
+        data: {
+            code: locals.user.firstName!.toUpperCase(),
+            time: date.toDate(),
+            user: {
+                connect: {id: memberId}
+            }
+        }
+    });
+
+    return json(attendance);
+}) satisfies RequestHandler;
+
+export const DELETE = (async ({request, locals}) => {
+    requireRank(
+        locals.user!, Role.CORPORAL,
+        [LeadershipDepartment.ADMINISTRATION, LeadershipDepartment.TRAINING]
+    );
+
+
+    const {id} = await request.json();
+    await db.attendance.delete({
+        where: {id}
+    });
+    return new Response("OK");
+}) satisfies RequestHandler;
