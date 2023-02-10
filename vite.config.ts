a859ab9d2ac6cab01e435cbc358abfc76f4aa0b0
David Kanter Eivin <david.kantereivin@sjacs.ca>
2023-02-09 21:43:30 -0500

    fix: correct error resolving module specifier due to Prisma
    prisma/prisma#12504

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';;
 import type { UserConfig } from 'vite';
 
+import { createRequire } from "module";
+
+const prismaPlugin = () => {
+	const require = createRequire(import.meta.url);
+	const pathName = require
+		.resolve("@prisma/client")
+		.replace("@prisma/client/index.js", "");
+
+	return {
+		name: "prisma-vite-plugin",
+		config: () => ({
+			resolve: {
+				alias: {
+					".prisma/client/index-browser": `${pathName}.prisma/client/index-browser.js`
+				}
+			}
+		})
+	};
+};
+
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), prismaPlugin()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
