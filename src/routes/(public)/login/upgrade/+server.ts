e27a2f4cd3451153589e0828af789dd0f40fea71
David Kanter Eivin <david.kantereivin@sjacs.ca>
2023-07-09 10:17:58 -0400

    fix: lodash import syntax corrected from prev commit

--- src/routes/(public)/login/upgrade/+server.ts
+++ src/routes/(public)/login/upgrade/+server.ts
@@ -2,7 +2,7 @@ import { SHEETS_SCOPE } from '$lib/server/sheets/common';
 import { redirect, RequestEvent } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private'
 import { OAuth2Client } from 'google-auth-library';
-import { isUndefined } from 'lodash';
+import _ from 'lodash';
 
 const defaultScope = [
 	'https://www.googleapis.com/auth/userinfo.email',
@@ -17,7 +17,7 @@ export function GET({url, locals}: RequestEvent): Response {
     const callback = url.searchParams.get('callback') ?? env.GOOGLE_OAUTH_CALLBACK_URI;
     const scopeNames = url.searchParams.get('scopes')?.split(',') ?? [];
 
-    const prompt = isUndefined(locals.user?.google?.refreshToken) ? 'consent' : undefined;
+    const prompt = _.isUndefined(locals.user?.google?.refreshToken) ? 'consent' : undefined;
 
     const client = new OAuth2Client(
         env.GOOGLE_CLIENT_ID,
